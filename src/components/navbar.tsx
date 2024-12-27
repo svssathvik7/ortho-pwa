7ebb76f6ca6ff051e1f9ebdf744b79286c730c35
SATHVIK SVS <svssathvik77@gmail.com>
2024-12-27 23:20:37 +0530

    improved navbar

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,10 +1,35 @@
 import { useAuthStore } from "@/store/authStore";
 import { Button } from "./ui/button";
 import { Link } from "react-router-dom";
+import axios from "axios";
+import { toast } from "@/hooks/use-toast";
 
+const backendUrl = import.meta.env.VITE_BACKEND_URL;
 export default function Navbar() {
     const isAuthenticated = useAuthStore((state)=>state.isAuthenticated);
-
+    const logout = useAuthStore((state)=>state.logout);
+    
+    const handleLogout = async()=>{
+        try {
+            await axios.post(`${backendUrl}/api/auth/logout`);
+            logout();
+            // Redirect to login page or update UI state
+            toast(
+                {
+                    title: "Successfully logged out!"
+                }
+            );
+            return;
+          } catch (error:any) {
+            console.error('Logout failed:', error);
+            toast(
+                {
+                    title: "Failed to logged out!",
+                }
+            );
+            return;
+          }
+    }
     return (
         <div className="flex items-center justify-around">
             <p>OrthoPWA</p>
@@ -14,10 +39,8 @@ export default function Navbar() {
                 <a href="/route3">Route3</a>
             </nav>
             {isAuthenticated ? (
-                <Button>
-                    <a className="px-4" href="#">
-                        Logout
-                    </a>
+                <Button className="px-4" onClick={handleLogout}>
+                    Logout
                 </Button>
             ) : (
                 <Button>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,14 @@
 import AssetUploader from "@/components/assetuploader";
 import Navbar from "@/components/navbar";
+import { useAuthStore } from "@/store/authStore";
 
 export default function Home(){
+    const isAuthenticated = useAuthStore((state)=>state.isAuthenticated);
+    const email = useAuthStore((state)=>state.email);
     return (
         <div>
             <Navbar/>
+            {isAuthenticated ? <h1 className="text-3xl font-bold absolute-centre w-fit h-fit">Welcome {email}</h1> : <h1 className="text-3xl font-bold absolute-centre w-fit h-fit">Welcome to Ortho PWA</h1>}
         </div>
     )
 }
