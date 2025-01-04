a852120c4f406f9374d0314f9d02dcd5d13349ca
SATHVIK SVS <svssathvik77@gmail.com>
2025-01-04 22:19:12 +0530

    update mobile version tag viewer

--- src/components/displayassets.tsx
+++ src/components/displayassets.tsx
@@ -7,6 +7,7 @@ import { Dialog, DialogContent, DialogHeader, DialogTitle } from "./ui/dialog";
 import { Badge } from "./ui/badge";
 import { Bolt, Hand, Split, Search } from "lucide-react";
 import { HoverCard, HoverCardContent, HoverCardTrigger } from "./ui/hover-card";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "./ui/tabs";
 
 const AssetGrid = () => {
   const email = useAuthStore((state) => state.email);
@@ -65,10 +66,13 @@ const AssetGrid = () => {
                 className="w-full aspect-square object-cover max-h-72 cursor-pointer"
                 onClick={() => setSelectedImage(image)}
               />
-              <div className="w-full flex items-center justify-around absolute top-0 left-0 right-0 opacity-0 group-hover:opacity-100 transition-opacity duration-300 p-2 bg-black bg-opacity-50">
+              <div className="hidden w-full lg:flex items-center justify-around absolute top-0 left-0 right-0 opacity-0 group-hover:opacity-100 transition-opacity duration-300 p-2 bg-black bg-opacity-50">
                 <HoverCard>
                   <HoverCardTrigger>
-                    <Badge title="Body parts" className="w-10 h-5 cursor-pointer">
+                    <Badge
+                      title="Body parts"
+                      className="w-10 h-5 cursor-pointer"
+                    >
                       <Hand />
                     </Badge>
                   </HoverCardTrigger>
@@ -77,17 +81,24 @@ const AssetGrid = () => {
                     align="center"
                     className="bg-white border border-gray-200 rounded-md p-2 shadow-lg text-xs text-gray-700 flex flex-col gap-1"
                   >
-                    <span className="text-black text-xl">{image.bodyParts.length == 0 && "No"} Body tags</span>
+                    <span className="text-black text-xl">
+                      {image.bodyParts.length == 0 && "No"} Body tags
+                    </span>
                     <div className="flex flex-wrap gap-1">
                       {image.bodyParts.map((part) => (
-                        <p className="rounded-full bg-blue-800 text-white px-2">{part}</p>
+                        <p className="rounded-full bg-yellow-500 text-white px-2">
+                          {part}
+                        </p>
                       ))}
                     </div>
                   </HoverCardContent>
                 </HoverCard>
                 <HoverCard>
                   <HoverCardTrigger>
-                    <Badge title="Implant Tags" className="w-10 h-5 cursor-pointer">
+                    <Badge
+                      title="Implant Tags"
+                      className="w-10 h-5 cursor-pointer"
+                    >
                       <Bolt />
                     </Badge>
                   </HoverCardTrigger>
@@ -96,17 +107,24 @@ const AssetGrid = () => {
                     align="center"
                     className="bg-white border border-gray-200 rounded-md p-2 shadow-lg text-xs text-gray-700"
                   >
-                    <span className="text-black text-xl">{image.implants.length == 0 && "No"} Implant tags</span>
+                    <span className="text-black text-xl">
+                      {image.implants.length == 0 && "No"} Implant tags
+                    </span>
                     <div className="flex flex-wrap gap-1">
                       {image.implants.map((implant) => (
-                        <p className="rounded-full bg-blue-800 text-white px-2">{implant}</p>
+                        <p className="rounded-full bg-yellow-500 text-white px-2">
+                          {implant}
+                        </p>
                       ))}
                     </div>
                   </HoverCardContent>
                 </HoverCard>
                 <HoverCard>
                   <HoverCardTrigger>
-                    <Badge title="Classification Tags" className="w-10 h-5 cursor-pointer">
+                    <Badge
+                      title="Classification Tags"
+                      className="w-10 h-5 cursor-pointer"
+                    >
                       <Split />
                     </Badge>
                   </HoverCardTrigger>
@@ -115,17 +133,25 @@ const AssetGrid = () => {
                     align="center"
                     className="bg-white border border-gray-200 rounded-md p-2 shadow-lg text-xs text-gray-700"
                   >
-                    <span className="text-black text-xl">{image.classifications.length == 0 && "No"} Classification tags</span>
+                    <span className="text-black text-xl">
+                      {image.classifications.length == 0 && "No"} Classification
+                      tags
+                    </span>
                     <div className="flex flex-wrap gap-1">
                       {image.classifications.map((classification) => (
-                        <p className="rounded-full bg-blue-800 text-white px-2">{classification}</p>
+                        <p className="rounded-full bg-yellow-500 text-white px-2">
+                          {classification}
+                        </p>
                       ))}
                     </div>
                   </HoverCardContent>
                 </HoverCard>
                 <HoverCard>
                   <HoverCardTrigger>
-                    <Badge title="Diagnosis Tags" className="w-10 h-5 cursor-pointer">
+                    <Badge
+                      title="Diagnoses Tags"
+                      className="w-10 h-5 cursor-pointer"
+                    >
                       <Search />
                     </Badge>
                   </HoverCardTrigger>
@@ -134,10 +160,14 @@ const AssetGrid = () => {
                     align="center"
                     className="bg-white border border-gray-200 rounded-md p-2 shadow-lg text-xs text-gray-700"
                   >
-                    <span className="text-black text-xl">{image.diagnoses.length == 0 && "No"} Diagnoses tags</span>
+                    <span className="text-black text-xl">
+                      {image.diagnoses.length == 0 && "No"} Diagnoses tags
+                    </span>
                     <div className="flex flex-wrap gap-1">
                       {image.diagnoses.map((diagnoses) => (
-                        <p className="rounded-full bg-blue-800 text-white px-2">{diagnoses}</p>
+                        <p className="rounded-full bg-yellow-500 text-white px-2">
+                          {diagnoses}
+                        </p>
                       ))}
                     </div>
                   </HoverCardContent>
@@ -158,15 +188,78 @@ const AssetGrid = () => {
             <DialogHeader>
               <DialogTitle>Asset Details</DialogTitle>
             </DialogHeader>
-            <div className="flex flex-col items-center">
+            <div className="flex flex-col items-center justify-center">
               <img
                 src={selectedImage.cloudinaryUrl}
                 alt={selectedImage.notes || "Asset Image"}
                 className="w-full max-h-96 object-contain"
               />
-              <div className="mt-4 text-sm text-gray-600">
-                {selectedImage.notes || "No additional notes provided."}
-              </div>
+              <Tabs className="w-full flex-wrap" defaultValue="body-parts">
+                <TabsList className="flex-wrap w-full m-2">
+                  <TabsTrigger className="w-1/4" value="body-parts">
+                    <Hand />
+                  </TabsTrigger>
+                  <TabsTrigger className="w-1/4" value="diagnoses">
+                    <Search />
+                  </TabsTrigger>
+                  <TabsTrigger className="w-1/4" value="classification">
+                    <Split />
+                  </TabsTrigger>
+                  <TabsTrigger className="w-1/4" value="implants">
+                    <Bolt />
+                  </TabsTrigger>
+                </TabsList>
+                <TabsContent value="body-parts">
+                  <span className="text-black text-xl">
+                    {selectedImage.bodyParts.length == 0 && "No"} Body tags
+                  </span>
+                  <div className="flex flex-wrap gap-1">
+                    {selectedImage.bodyParts.map((part) => (
+                      <p className="rounded-full bg-yellow-500 text-white px-2">
+                        {part}
+                      </p>
+                    ))}
+                  </div>
+                </TabsContent>
+                <TabsContent value="diagnoses">
+                  <span className="text-black text-xl">
+                    {selectedImage.diagnoses.length == 0 && "No"} Diagnoses tags
+                  </span>
+                  <div className="flex flex-wrap gap-1">
+                    {selectedImage.diagnoses.map((diagnoses) => (
+                      <p className="rounded-full bg-yellow-500 text-white px-2">
+                        {diagnoses}
+                      </p>
+                    ))}
+                  </div>
+                </TabsContent>
+                <TabsContent value="classification">
+                  <span className="text-black text-xl">
+                    {selectedImage.classifications.length == 0 && "No"}{" "}
+                    Classification tags
+                  </span>
+                  <div className="flex flex-wrap gap-1">
+                    {selectedImage.classifications.map((classification) => (
+                      <p className="rounded-full bg-yellow-500 text-white px-2">
+                        {classification}
+                      </p>
+                    ))}
+                  </div>
+                </TabsContent>
+                <TabsContent value="implants">
+                  <span className="text-black text-xl">
+                    {selectedImage.implants.length == 0 && "No"}{" "}
+                    Implant tags
+                  </span>
+                  <div className="flex flex-wrap gap-1">
+                    {selectedImage.implants.map((implant) => (
+                      <p className="rounded-full bg-yellow-500 text-white px-2">
+                        {implant}
+                      </p>
+                    ))}
+                  </div>
+                </TabsContent>
+              </Tabs>
             </div>
           </DialogContent>
         </Dialog>
